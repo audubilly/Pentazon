61c0b59d0455bf29ea116d2e934d2333f8b22112
Audu Billy <audubilly89@gmail.com>
2021-03-14 11:09:06 +0100

    Pentazon updates

--- Tests/com/pentazon/Shopping/CartTest.java
+++ Tests/com/pentazon/Shopping/CartTest.java
@@ -24,23 +24,53 @@ class CartTest {
 
     @Test
     void addToCart(){
-        assertTrue(cart.getProducts().isEmpty());
-         Product testProduct = new Product();
-         cart.addToCart(testProduct);
-         assertFalse(cart.getProducts().isEmpty());
+        assertTrue(cart.getItems().isEmpty());
+         Product plantainChips  = new Product("Adunni chips ", "Savoury PlantainChips", new BigDecimal(50));
+         cart.addToCart(plantainChips);
+         plantainChips.setProductId("AD001");
+         cart.addToCart(plantainChips);
+         assertFalse(cart.getItems().isEmpty());
+         assertEquals(1,cart.getItems().size());
     }
 
     @Test
     void removeFromCart(){
-        assertTrue(cart.getProducts().isEmpty());
+        assertTrue(cart.getItems().isEmpty());
         Product plantainChips = new Product("Adunni Chips","Savoury plantainChips", new BigDecimal(100));
         plantainChips.setProductId("ADOO1");
         cart.addToCart(plantainChips);
-       assertFalse(cart.getProducts().isEmpty());
-       assertEquals(1,cart.getProducts().size());
+       assertFalse(cart.getItems().isEmpty());
+       assertEquals(1,cart.getItems().size());
 
        boolean result = cart.removeFromCart(plantainChips);
        assertTrue(result);
-       assertTrue(cart.getProducts().isEmpty());
+       assertTrue(cart.getItems().isEmpty());
+    }
+
+    @Test
+    void calculateTotal(){
+        assertTrue(cart.getItems().isEmpty());
+        Product plantainChips = new Product("Adunni Chips","Savoury plantainChips", new BigDecimal(50));
+        plantainChips.setProductId("ADOO1");
+        cart.addToCart(plantainChips);
+
+        Product shirt = new Product("Vintage shirts","Vintage versace shirts", new BigDecimal(5000));
+        shirt.setProductId("AD003");
+        cart.addToCart(shirt);
+        assertFalse(cart.getItems().isEmpty());
+        assertEquals(2,cart.getItems().size());
+        BigDecimal cartTotal = cart.calculateTotal();
+        assertEquals(5050,cartTotal.intValue());
+
+        CartItem chipsItem = cart.getItems().get(plantainChips.getProductId());
+        assertEquals(1,chipsItem.getQuantity());
+        cart.addToCart(plantainChips,9);
+        assertEquals(2,cart.getItems().size());
+        chipsItem = cart.getItems().get(plantainChips.getProductId());
+        assertEquals(10,chipsItem.getQuantity());
+
+        boolean result = cart.removeFromCart(plantainChips);
+        assertTrue(result);
+        assertTrue(cart.getItems().isEmpty());
     }
 }
--- src/com/pentazon/Shopping/Cart.java
+++ src/com/pentazon/Shopping/Cart.java
@@ -5,36 +5,51 @@ import com.pentazon.Product.ProductService;
 import com.pentazon.Product.ProductServiceImpl;
 import com.pentazon.ProductExceptions;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.logging.Logger;
 
 public class Cart {
     private Logger logger = Logger.getLogger(Cart.class.getName());
     private Map<String, CartItem> items;
-   private ProductService productService = new ProductServiceImpl();
+    private ProductService productService;
+    private BigDecimal total = BigDecimal.ZERO;
 
-    public Cart(){
+
+    public Cart() {
         items = new HashMap<>();
-        productService = new ProductServiceImpl() ;
+        productService = new ProductServiceImpl();
 
     }
 
-    public void addToCart(Product product){
-        if(verifiedProduct(product)){
+    public void addToCart(Product product) {
+        if (verifiedProduct(product)) {
             CartItem item = items.get(product.getProductId());
-            if(item == null){
+            if (item == null) {
                 item = new CartItem(product);
 
             }
             item.addItems(BigInteger.ONE.intValue());
-                this.items.put(product.getProductId(), item);
-            }
-
+            this.items.put(product.getProductId(), item);
         }
+
+    }
+
+    public void addToCart(Product product, int quantity) {
+        if (verifiedProduct(product)) {
+            CartItem item = items.get(product.getProductId());
+            if (item == null) {
+                item = new CartItem(product);
+
+            }
+            item.addItems(quantity);
+            this.items.put(product.getProductId(), item);
+        }
+
+    }
+
+
 
 
 
@@ -59,11 +74,30 @@ public class Cart {
         return removed;
     }
 
-    public List<Product> getProducts() {
+    public BigDecimal calculateTotal() {
+        if(!items.isEmpty()){
+            this.total = BigDecimal.ZERO;
+            Iterator<CartItem> cartItems = items.values().iterator();
+            while(cartItems.hasNext()){
+                this.total = this.total.add(cartItems.next().getTotal());
+            }
+        }
+        return this.getTotal();
+    }
+
+    public Map<String, CartItem> getItems() {
         return items;
     }
 
-    public void setProducts(List<Product> products) {
-        this.items = products;
+    public void setItems(Map<String, CartItem> items) {
+        this.items = items;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
     }
 }
